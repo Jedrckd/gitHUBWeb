0ca24ba3234357471f841e99f832e54262b442f1
Jedrckd <lems.jedrickgonzales@gmail.com>
2025-01-09 00:04:57 +0800

    Add files via upload

---
+++ DiGo.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Discover the Philippines: Your Ultimate Travel Guide</title>
+    <link rel="stylesheet" href="IPJED22.css">
+</head>
+<body>
+
+<nav class="navbar">
+        <div class="navdiv">
+            <div class="logo"><a href="https://uk.pinterest.com/pin/945474515495294418"><b>Explore the Philippines </b></a></div>
+            <ul>
+                <li><a href="DiGo.html">Home</a></li>
+                <li><a href="DiGo.html#attractions">Attractions</a></li>
+                <li><a href="DiGo.html#culture">Culture</a></li>
+                <li><a href="DiGo.html#contact">Contacts</a></li>
+            </ul>
+    </nav>
+
+<center><BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+
+<h1>We appreciate you contacting us <?php echo $_GET['name3'];?>, and we will work to make our website better.<br></h1>
+
+
+<BR></center></body>
+</html>
